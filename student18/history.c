3586a9b10c4f20a527a16424799f3c4dc17ca2f1
Melchor Dominguez <mdominguez1@agora.cs.wcu.edu>
2018-03-16 17:09:28 -0400

    okay

--- student18/history.c
+++ student18/history.c
@@ -1,25 +1,41 @@
-#include <stdio.h>
-#define HIST_SIZE 40
-// TODO: Stuff in here to save and list history
+#include "history.h"
 
-/**
- * Will add a command line to the history
- * @param args - arguments from the command line
- * @param hist - array which holds history 
- *
-void add_history(char* args, char hist[]){
-    int length = hist.size();
-    hist[length - 1] = args;
-} */
+int add_Hist(char* line){
+    
+    /**initiliaze a static history variable to hold command lines*/
+    static char* hist[HSIZE];
 
-/**
- * Will execute the history command 
- *
-void do_history(char* args, char hist[]){
-    add_history(args,hist);
-    int i;
-    for(i = 0; i < HIST_SIZE; i++){
-        printf(stdout,hist[i] + "\n");
+    /**initialize a static index to keep track of the element number*/
+    static int index = 0;
 
-    }
-} */ 
+    if(line == NULL){
+        int i = 0;
+        while(i < index){
+            printf(hist[i]);
+            printf("\n");
+            fflush(stdout);
+            i++;
+        }//end while
+        return EXIT_SUCCESS;
+    }//end if
+    
+    if(index < HSIZE){
+        hist[index] = line;
+        index++;
+    }else{
+        
+        int elem = 1;
+        while(elem < HSIZE){
+            if(elem == HSIZE -1){
+                hist[elem - 1] = hist[elem];
+                hist[elem] = line;
+            }else{
+                hist[elem - 1] = hist[elem];
+            }//end if-else
+            elem++;
+        }//end while
+    }//end if-else
+
+    return EXIT_SUCCESS;
+
+}
