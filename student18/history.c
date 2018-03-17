69cba4734e8ca3b4944d0bbe311fea3d94177bf6
Melchor Dominguez <mdominguez1@agora.cs.wcu.edu>
2018-03-16 21:04:23 -0400

    commenting

--- student18/history.c
+++ student18/history.c
@@ -1,5 +1,13 @@
 #include "history.h"
 
+/**
+ * History will initialize an array to hold command line arguments
+ * if NULL is the parameter, the history of command line arguments 
+ * will be printed
+ * @param line - the command line to be entered into history
+ *             - if NULL, print history
+ * @return - 0 upon success
+ */
 int add_Hist(char* line){
     
     /**initiliaze a static history variable to hold command lines*/
@@ -8,22 +16,25 @@ int add_Hist(char* line){
     /**initialize a static index to keep track of the element number*/
     static int index = 0;
 
+    //if NULL is entered print
     if(line == NULL){
-        int i = 0;
-        while(i < index){
-            printf(hist[i]);
+
+        /** element of array to be printed*/
+        int elem = 0;
+        while(elem < index){
+            printf(hist[elem]);
             printf("\n");
             fflush(stdout);
-            i++;
+            elem++;
         }//end while
         return EXIT_SUCCESS;
     }//end if
     
+    //add command line to history
     if(index < HSIZE){
         hist[index] = line;
         index++;
     }else{
-        
         int elem = 1;
         while(elem < HSIZE){
             if(elem == HSIZE -1){
@@ -35,7 +46,5 @@ int add_Hist(char* line){
             elem++;
         }//end while
     }//end if-else
-
     return EXIT_SUCCESS;
-
 }
